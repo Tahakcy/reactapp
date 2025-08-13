d70d1353e2e7447aeee0df3619d37235e45714dc
Tahakcy <5greceptahaakcay@gmail.com>
2025-08-13 12:40:50 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,34 +1,67 @@
-import logo from './logo.svg';
 import { useEffect, useState } from 'react';
 import './App.css';
-import Product from './product.js';
 const URL = 'http://localhost:5000/todoList';
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
 
   useEffect(() => {
-    function loadTodos() {
-      fetch(URL)
-      .then(res => res.json())
-      .then(todos => setTodos(todos));
-    } 
     loadTodos();
-  }, []);
+  })
+
+  const loadTodos = () => {
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => setTodos(data));
+  };
+
+  const handleAddTodo = ()  => {
+    if(!inputValue.trim()){
+      alert('Boş görev eklenemez!');
+      return;
+    }
+  } 
+
+  fetch(URL, {
+    method: 'POST',
+    headers: {'Conten-Type':'application/json'},
+    body: JSON.stringify({task:inputValue, completed:false})
+  })
+  .then(() => {
+    setInputValue('');
+    loadTodos();
+  });
+
+  const handleToggleComplete = (id, task, completed) => {
+    fetch(URL+'/'+id, {
+      method:'PUT',
+      headers: {'Conten-Type':'application/json'},
+      body: JSON.stringify({
+        task:todo.task,
+        completed:checkbox.checked
+      })
+    })
+    .then(() => loadTodos())
+  }
+  
+  const handleDeleteTodon = (id) => {
+    fetch(URL+'/'+id, {
+      method:'DELETE'
+    })
+    .then(() => loadTodos())
+  }
+} 
 
     return (
       <div>
         <h1>To-do App</h1> 
         <ul>
-          {todos.map(todo => (
-            <li key={todo.id}>
-              <input type='checkbox' checked={todo.completed}/>
-              {todo.task}
-            </li>
-          ))}
+            <li>
+            </li> 
         </ul>
       </div>
     );
-}
+
 
 export default App;
