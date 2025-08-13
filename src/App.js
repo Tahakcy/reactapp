030f71a8f8804f64553e078b8c24e51d47c55fd0
Tahakcy <5greceptahaakcay@gmail.com>
2025-08-13 11:17:23 +0300

    todoList

--- src/App.js
+++ src/App.js
@@ -2,96 +2,33 @@ import logo from './logo.svg';
 import { useEffect, useState } from 'react';
 import './App.css';
 import Product from './product.js';
-
+const URL = 'http://localhost:5000/todoList';
 
 function App() {
-
-
-  const [firstName, setFirstName] = useState('Taha')
-  const [names, setNames] = useState(['isim 1', 'isim 2', 'isim 3'])
-  const [userInfo, setUserInfo] = useState({username: 'tahakcy', password: '123123'})
-  const [count, setCount] = useState(0);
-  const [productName, setProductName] = useState('');
-  const [productPrice, setPrice] = useState('');
-
-    const nameChange = () => {
-    setFirstName("İsim değişti")
-  }
-
-  const changeNames = () => {
-    setNames([
-      'yeni 1', 'yeni 2', 'yeni 3'
-    ])
-  }
-
-  const newInfo = () => {
-    setUserInfo({
-      username:'yeni username',
-      password:'1111111'
-    })
-  }
-
-  const increase = () => {
-    setCount(count+1)
-  }
-
-  const decrease = () => {
-    if(count > 0){
-    setCount(count-1)
-    }
-  }
+  const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    console.log('Her zaman çalışır.')
-  })
-
-  useEffect(() => {
-    console.log('İlk render edilişte çalışır.')
-  }, [])
-
-  useEffect(() =>{
-    console.log('ilk render ve firstname değiştiğinde')
-  },[firstName])
-
-   useEffect(() =>{
-    console.log('ilk render ve name list değiştiğinde')
-  },[names])
-
-   useEffect(() =>{
-    console.log('ilk render ve user info değiştiğinde')
-  },[userInfo])
-
-  useEffect(() => {
-    console.log('ilk render ve count değişince çalışır')
-  },[count])
-
-   useEffect(() => {
-    console.log('İlk render ve product propsu değiştiğinde.')
-  },[productName])
-
-  useEffect(() => {
-    console.log('İlk render ve product propsu değiştiğinde.')
-  },[productPrice])
+    function loadTodos() {
+      fetch(URL)
+      .then(res => res.json())
+      .then(todos => setTodos(todos));
+    } 
+    loadTodos();
+  }, []);
 
     return (
-    <div>
-      {firstName}
-      <button onClick={nameChange}>isim değiştir 1</button>
-      <button onClick={()=> setFirstName("yeni isim")}>isim değiştir 2</button>
-      {names.map((name ,index) => (
-        <div key = {index}>{name}</div>
-      ))}
-      {userInfo.username} {userInfo.password}
-      <p>Count: {count}</p>
-      <button onClick={increase}>increase</button>
-      <button onClick={decrease}>decrease</button>
-      <button onClick={changeNames}>isim dizisini değiş</button>
-      <button onClick={newInfo}>info değiş</button>
-      <Product name={productName} price ={productPrice}/>
-      <button onClick={() => setProductName('ürün ismi')}>product name</button>
-      <button onClick={() => setPrice('6444 TL')}> product price</button>
-    </div>
-  );
+      <div>
+        <h1>To-do App</h1> 
+        <ul>
+          {todos.map(todo => (
+            <li key={todo.id}>
+              <input type='checkbox' checked={todo.completed}/>
+              {todo.task}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
 }
 
 export default App;
