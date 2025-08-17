c8671f30c15f7b409574edfbc45eb0dc1a46d2ea
Tahakcy <5greceptahaakcay@gmail.com>
2025-08-17 13:05:13 +0300

    put, delete

--- src/App.js
+++ src/App.js
@@ -21,13 +21,12 @@ function handleAdd(event){
 
   const formData = new FormData(event.target);
   const taskText = (formData.get("taskInput") || "").toString().trim();
-
   if(!taskText) return;
 
   fetch(URL, {
     method: "POST",
     headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({ task: text, completed: false }),
+    body: JSON.stringify({ taskText: text, completed: false }),
   })
   .then(() => {
     event.target.reset();
@@ -36,24 +35,63 @@ function handleAdd(event){
   .catch((err) => console.error('Eklenemedi: ', err));
 }
 
+function handleTask(event, todo){
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const action = formData.get("action");
+  const completed = formData.get("completed") === "on";
+
+  if(action === "delete"){
+    fetch(URL+"/"+todo.id, {
+      method: "DELETE",
+    })
+    .then(loadTodos)
+    .catch((err) => console.error("Delete error:", err));
+  return;
+  }
+
+  if (action === null){
+    fetch(URL + "/" +todo.id, {
+      method:"PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ completed }),
+    })
+    .then(loadTodos)
+    .catch((err) => console.error("Edit error: ", err))
+    return;
+  }
+
+  if(action === "edit"){
+
+  }
+}
+
   return (
     <div>
       <h1>Todo App</h1><hr></hr>
-      
+
        <form onSubmit={handleAdd}>
         <input type="text" name="taskInput" placeholder="Add new task..."/>
         <button type="submit">Add</button>
        </form>
 
+       
+
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
-            <form>
-              <input type="checkbox" checked={todo.completed}/>
-              <span>{todo.task}</span>
-              <button type="submit" name="action" value="edit">Edit</button>
-              <button type="submit" name="action" value="delete">Delete</button>
-            </form>
+            <form onSubmit={(event) => handleTask(event,todo)}>
+              <input
+                type="checkbox"
+                name="completed"
+                defaultChecked={todo.completed}
+                onChange={(event) => event.target.form.requestSubmit()}
+              />
+          <span>{todo.task}</span>
+          <button type="submit" name="action" value="edit">Edit</button>
+          <button type="submit" name="action" value="delete">Delete</button>
+       </form>
           </li>
         ))}
       </ul>
